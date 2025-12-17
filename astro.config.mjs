6e339dfd7e897c18b27add52fe2538c68ace4165
Charlie373 <charlie.1b2b3@gmail.com>
2025-12-17 13:26:49 +0000

    Add site URL for sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://seeded-media-astro.netlify.app/',
+
   vite: {
     plugins: [tailwindcss()]
   },
