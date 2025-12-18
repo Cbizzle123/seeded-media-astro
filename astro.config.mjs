5116124e2e492444cccb05fb55b0105f1aa6076b
Charlie373 <charlie.1b2b3@gmail.com>
2025-12-18 16:53:06 +0000

    sitemap fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://seeded-media-astro.netlify.app/',
+  site: 'https://seededmedia.co.uk',
 
   vite: {
     plugins: [tailwindcss()]
